44aff078d063b4fe87e9a95a0ca91161851d606c
Shivam Chaurasia <shivam.d.only1@gmail.com>
2016-03-18 15:18:52 +0530

    Long and Lat hardcoded

--- client/init.js
+++ client/init.js
@@ -2,6 +2,8 @@ Meteor.startup(function() {
   // Potentially prompts the user to enable location services. We do this early
   // on in order to have the most accurate location by the time the user shares
   Geolocation.currentLocation();
+  Session.set('LAT',51.5034070);
+  Session.set('LON',-0.1275920);
   navigator.geolocation.getCurrentPosition(function(loc) {
   	Session.set('LAT',loc.coords.latitude);
   	Session.set('LON',loc.coords.longitude);
