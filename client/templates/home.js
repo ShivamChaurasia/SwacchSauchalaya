915e1a4dddb0cd675151e9ec76aa82e700e30d23
Shivam Chaurasia <shivam.d.only1@gmail.com>
2016-03-19 15:34:02 +0530

    home Page

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,3 +1,23 @@
+Meteor.startup(function() {
+  Session.set("languageSelected", false);
+  Session.set("SELECTEDLOO", "RajivChowk_Metro_Gate_1");
+});
 Template.home.helpers({
+  languageSelected: function(){
+    return Session.get("languageSelected");
+  },
+  classforPopUp: function(){
+    if (Session.get("languageSelected")) {
+      return "hide";
+    } else {
+      return "popupWrapper"
+    }
+  }
+});
 
+Template.home.events({
+  'click input': function(event, template){
+    console.log(event);
+    Session.set("languageSelected", true);
+  }
 });
